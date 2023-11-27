d434316de3e0363bf7176d79f6ef4a79f1239967
windsywinds <windsor.sam@gmail.com>
2023-11-28 00:45:58 +1300

    routetest only

--- server.js
+++ server.js
@@ -4,25 +4,12 @@ const cors = require("cors")
 const bodyParser = require("body-parser");
 
 const routetest = require ("./routes/routetest")
-const calculatevalue = require("./routes/calculateValue")
-const calculaterisk = require("./routes/calculateRisk")
 
 const server = express();
 
+server.use(bodyParser.json());
 server.use(cors());
-server.get("/", (req, res) => {
-  res.send(`
-    <h1>API1</h1>
-    <p>The app is operable:</p>
-    <p>/routetest/</p> Working
-    <p>/calculatevalue/</p> not working
-    <p>/calculaterisk/</p> not working
-  `);
-});
-
 server.use("/routetest/", routetest);
-server.use("/calculatevalue/", calculatevalue);
-server.use("/calculaterisk/", calculaterisk);
 
 //Allow a test route and response to ensure connection and operation between testing and server
 server.use("/test", (req, res) => {
