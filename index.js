3eb099a6a0c745e2268faa213e08c77462fd6068
windsywinds <windsor.sam@gmail.com>
2023-11-28 02:22:08 +1300

    init new

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+const express = require("express");
+const cors = require("cors")
+const bodyParser = require("body-parser");
 
-const { server } = require('./server'); 
+const routetest = require ("./routes/routetest")
 
 
-
+const server = express();
 const PORT = process.env.PORT || 8000;
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send(`
+  <h1>The API</h1>  
+`);
+});
+
+server.use("/routetest/", routetest);
+
+//Allow a test route and response to ensure connection and operation between testing and server
+server.use("/test", (req, res) => {
+  //.use to allow both post and get
+  const testResponse = "You're receiving a response from Azure!";
+  res.json(testResponse);
+});
+
 
 server.listen(PORT, () => {
   console.log(`Server started and listening on port ${PORT}`);
