325af503de21b514a3759b0bd1adaa88efc72aa9
windsywinds <windsor.sam@gmail.com>
2023-11-27 19:51:52 +1300

    old yml

--- routes/calculateRisk.js
+++ routes/calculateRisk.js
@@ -5,6 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const calculaterisk = express.Router();
+calculatevalue.use(bodyParser.json());
 
 
 
@@ -16,17 +17,6 @@ calculaterisk.post("/", async (req, res) => {
       res.status(400).json({ error: "there is an error" });
     }
   try {
-    const riskResult = calculateRisk(claim_history);
-    res.json({ risk_rating: riskResult });
-  } catch (error) {
-    res.status(400).json({ error: "there is an error" });
-  }
-
-  });
-
-  //Find the risk rating from the users input for API2
-function calculateRisk(input) {
-    //List an array of words to use as triggers
     const triggerWords = ["collide", "crash", "scratch", "bump", "smash"];
     let count = 0;
     // Split the user input into words
@@ -48,7 +38,12 @@ function calculateRisk(input) {
     if (count >= 6) {
       count = 5;
     }
-    return count;
+    const riskResult = count;
+    res.json({ risk_rating: riskResult });
+  } catch (error) {
+    res.status(400).json({ error: "there is an error" });
   }
+
+  });
 
 module.exports = calculaterisk;
